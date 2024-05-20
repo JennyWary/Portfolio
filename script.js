a7b7a5b5de2b7c1135b2feb920b28d92c08dc007
JennyWary <j.bramadas@outlook.de>
2024-05-20 14:05:08 +0200

    Schriftfarbe und image änderung wenn man scrollt

---
+++ script.js
@@ -0,0 +1,25 @@
+window.addEventListener('scroll', function() {
+    var headerTexts = document.querySelectorAll('.color-text');
+    var scrollPosition = window.scrollY;
+
+    headerTexts.forEach(function(headerText) {
+        if (scrollPosition > 200) {
+            headerText.style.color = '#5F00C2'; // Neue Schriftfarbe beim Scrollen
+        } else {
+            headerText.style.color = '#C20000'; // Ursprüngliche Schriftfarbe
+        }
+    });
+});
+
+window.addEventListener('scroll', function() {
+    var scrollImages = document.querySelectorAll('.scroll-image');
+    var scrollPosition = window.scrollY;
+
+    scrollImages.forEach(function(image, index) {
+        if (scrollPosition > 200) {
+            image.src = `../imgs/image${index + 4}.png`; // Ändere das Bild auf eine neue URL beim Scrollen
+        } else {
+            image.src = `../imgs/image${index + 1}.png`; // Ursprüngliches Bild
+        }
+    });
+});
